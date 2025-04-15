36d5892eed30f22cbf3ff84a340afefbd601a88b
F-Limas <flaviolimaetb@gmail.com>
2025-04-15 00:02:51 -0300

    Criacao da classe conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        float saldo = 237.48f;
+
+        System.out.print("Por favor, digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.print("Por favor, digite o número da Conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n", nomeCliente, agencia, conta, saldo);
+
+
     }
 }
